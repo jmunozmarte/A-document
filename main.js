795ddd7a6c98d3ff7863f66accc6353e4cca52f2
jmunozmarte <131613600+jmunozmarte@users.noreply.github.com>
2023-04-23 14:18:05 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let valor1, valor2;
+valor1 = 30;
+valor2 = 10;
+
+console.log(valor1 + valor2);
+
+console.log(valor1 - valor2);
+
+console.log(valor1 * valor2);
+
+console.log(valor1 / valor2);
+
+document.write("Hello world");
+document.write("<br>");
+document.write("goodbye world");
